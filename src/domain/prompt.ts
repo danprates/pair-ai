c050fc3cdd34ccd01adc4945d6528bc2d661595a
Daniel Prates <github@daniel-prates.dev>
2025-03-01 00:23:01 -0300

    refactor: Use Bun file API

--- src/domain/prompt.ts
+++ src/domain/prompt.ts
@@ -1,16 +1,13 @@
-import { writeFile } from "fs/promises";
-
 export class Prompt {
   private constructor(public content: string) {}
 
   static async from(file: string): Promise<Prompt> {
-    // const content = await readFile(file, "utf-8");
     const content = await Bun.file(file).text();
     return new Prompt(content);
   }
 
   static async to(file: string, content: string): Promise<void> {
-    await writeFile(file, content, "utf-8");
+    await Bun.write(file, content);
   }
 
   public replace(key: string, value: string): void {
