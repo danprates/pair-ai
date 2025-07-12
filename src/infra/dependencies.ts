812085dbff7108891972912c84ea0dbd69b30640
Daniel Prates <github@daniel-prates.dev>
2025-07-12 16:16:07 -0300

    refactor: git to functional

--- src/infra/dependencies.ts
+++ src/infra/dependencies.ts
@@ -1,10 +1,13 @@
 import type { DependencyInjection } from "../domain/types";
 import { Gemini } from "./artificial-inteligence/gemini";
 import { Console } from "./console";
-import { Git } from "./git";
-
+import { commit, getDiff, getLogs } from "./git";
 export const dependencies: DependencyInjection = {
-  git: new Git(),
+  git: {
+    getDiff,
+    commit,
+    getLogs,
+  },
   console: new Console(),
   ai: new Gemini(process.env.GEMINI_API_KEY || "", "gemini-2.0-flash"),
 };
--- src/infra/git.ts
+++ src/infra/git.ts
@@ -1,20 +1,18 @@
 import { $ } from "bun";
-import type { Git as GitInterface } from "../domain/types";
 
-export class Git implements GitInterface {
-  async getDiff(): Promise<string> {
-    const { stdout } =
-      await $`git add . && git diff --cached --diff-filter=ACMR`.quiet();
-    return stdout.toString().trim();
-  }
-  async commit(message: string): Promise<void> {
-    const sanitizedMessage = message.replaceAll("```", "").trim();
-    await $`git add . && git commit -m "${sanitizedMessage}"`;
-  }
+export const getDiff = async (): Promise<string> => {
+  const { stdout } =
+    await $`git add . && git diff --cached --diff-filter=ACMR`.quiet();
+  return stdout.toString().trim();
+};
 
-  async getLogs(branch: string): Promise<string> {
-    const { stdout } =
-      await $`git log --patch --graph ${branch}.. --diff-filter=ACMR`.quiet();
-    return stdout.toString().trim();
-  }
-}
+export const commit = async (message: string): Promise<void> => {
+  const sanitizedMessage = message.replaceAll("```", "").trim();
+  await $`git add . && git commit -m "${sanitizedMessage}"`;
+};
+
+export const getLogs = async (branch: string): Promise<string> => {
+  const { stdout } =
+    await $`git log --patch --graph ${branch}.. --diff-filter=ACMR`.quiet();
+  return stdout.toString().trim();
+};
