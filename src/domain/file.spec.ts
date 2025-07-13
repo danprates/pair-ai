50333f743c1aa6b43f74c72f832356516f238736
Daniel Prates <github@daniel-prates.dev>
2025-07-13 11:35:08 -0300

    refactor: prompt to functional

---
+++ src/domain/file.spec.ts
@@ -0,0 +1,41 @@
+import { afterAll, beforeEach, describe, expect, it } from "bun:test";
+import { rm, writeFile } from "fs/promises";
+import { readFile, replaceKey } from "./file";
+
+describe("File functions", () => {
+  beforeEach(async () => {
+    await writeFile(
+      "test.xml",
+      "<prompt><name>Hello {{  name}}!</name></prompt>"
+    );
+  });
+
+  afterAll(async () => {
+    await rm("test.xml");
+  });
+
+  describe("readFile", () => {
+    it("should throw error when file is not found", () => {
+      expect(readFile("unknown.xml")).rejects.toThrow();
+    });
+
+    it("should return a prompt object", async () => {
+      const prompt = await readFile("test.xml");
+      expect(prompt).toBe("<prompt><name>Hello {{  name}}!</name></prompt>");
+    });
+  });
+
+  describe("replaceKey", () => {
+    it("should replace the key with the value", async () => {
+      const file = await readFile("test.xml");
+      const prompt = replaceKey(file, "name", "world");
+      expect(prompt).toBe("<prompt><name>Hello world!</name></prompt>");
+    });
+
+    it("should not replace the key if it is not found", async () => {
+      const file = await readFile("test.xml");
+      const prompt = replaceKey(file, "unknown", "world");
+      expect(prompt).toBe("<prompt><name>Hello {{  name}}!</name></prompt>");
+    });
+  });
+});
